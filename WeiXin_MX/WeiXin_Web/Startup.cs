63ea2b55085b6a5cd0eaac232a3a16b641a4b420
马鑫 <anyangmaxin@163.com>
2015-03-13 11:50:41 +0800

    创建空的网站应用程序

---
+++ WeiXin_MX/WeiXin_Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeiXin_Web.Startup))]
+namespace WeiXin_Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
