8df5a085a20791e4f2f8788e83dd443402a02fcf
马鑫 <anyangmaxin@163.com>
2015-03-18 10:49:30 +0800

    添加注释以及关注事件枚举类型
    把所事件类型统一到一个类，以及所有缓存名称统一到一个类

--- WeiXin_MX/WX_Tools/AllCach.cs
+++ WeiXin_MX/WX_Tools/AllCach.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace WX_Tools
 {
+
+    /// <summary>
+    /// 整个项目中用到的缓存名称，保持全局统一
+    /// </summary>
    public class AllCach
     {
        public AllCach() { }
---
+++ WeiXin_MX/WX_Tools/AllEventEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WX_Tools
+{
+    /// <summary>
+    /// 全局所用的统一名称，枚举方式记录，包括事件等
+    /// </summary>
+  public  class AllEventEnum
+    {
+      public enum EventEnum
+      {
+          /// <summary>
+          /// 订阅（关注）事件
+          /// </summary>
+          subscribe,
+
+          /// <summary>
+          /// 取消订阅（取消关注）事件
+          /// </summary>
+          unsubscribe
+      }
+    }
+}
