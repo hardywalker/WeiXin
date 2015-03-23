7340585eb398a644bf6f9fc27eb047238a68bf9b
马鑫 <anyangmaxin@163.com>
2015-03-23 11:13:47 +0800

    添加上传图片的一般处理程序

---
+++ WeiXin_MX/WeiXin_Web/ImageUpload.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeiXin_Web
+{
+    /// <summary>
+    /// ImageUpload 的摘要说明
+    /// </summary>
+    public class ImageUpload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
