53316885ad28779bc5f546a07c2c8fd3a147e181
马鑫 <anyangmaxin@163.com>
2015-04-01 16:58:09 +0800

    完成XML文件操作，传入目录以及文件名称，就可以创建和读取内容。

---
+++ WeiXin_MX/WX_Tools/XmlReadWrite.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WX_Tools
+{
+    /// <summary>
+    /// Xml文件读写类
+    /// </summary>
+    public class XmlReadWrite
+    {
+        /// <summary>
+        /// 根据相对路径以及文件全名读取xml文件，需要传入实体对象类型   反序列化方式
+        /// </summary>
+        /// <param name="filePath">相对路径(/aa/aa/)</param>
+        /// <param name="obj">实体对象类型</param>
+        /// <param name="fileName">文件名（a.txt）</param>
+        /// <returns>返回对象</returns>
+        public Object Read(string filePath, object obj, string fileName)
+        {
+            //获取物理路径 
+            filePath = HttpContext.Current.Server.MapPath(filePath);
+            //判断路径是否存在，不存在创建路径 
+            if (Directory.Exists(filePath))
+            {
+                filePath = Path.Combine(filePath, fileName);
+                //根据路径 ，把文件放入文件流
+                FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+                XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+                return xmlSerializer.Deserialize(fileStream);
+            }
+            return null;
+        }
+
+
+        /// <summary>
+        /// 根据相对路径来生成xml文件，需要传入实体对象
+        /// </summary>
+        /// <param name="filePath">相对路径(/aa/aaa/)</param>
+        /// <param name="obj">实体对象</param>
+        /// <param name="fileName">文件全名(a.config)</param>
+        public void Write(string filePath, object obj, string fileName)
+        {
+            //获取物理路径 
+            filePath = HttpContext.Current.Server.MapPath(filePath);
+            //判断路径是否存在，不存在创建路径 
+            DirectoryIsExists(filePath);
+            filePath = Path.Combine(filePath, fileName);
+            FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+            xmlSerializer.Serialize(fileStream, obj);
+
+        }
+
+
+        /// <summary>
+        /// 判断xml所在文件夹是否存在，不存在的话，就创建此路径，注意传入的路径是物理路径
+        /// </summary>
+        /// <param name="filePath">物理路径</param>
+        private void DirectoryIsExists(string filePath)
+        {
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+        }
+
+
+    }
+}
--- WeiXin_MX/WeiXin_Web/Set.aspx.cs
+++ WeiXin_MX/WeiXin_Web/Set.aspx.cs
@@ -1,33 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Xml.Serialization;
+using WX_Tools;
 using WX_Tools.Entites;
 
 namespace WeiXin_Web
 {
-    public partial class Set : System.Web.UI.Page
+    public partial class Set : Page
     {
-       AppidSecretToken appidSecret=new AppidSecretToken();
-        private string filePath;
+       AppidSecretToken _appidSecret=new AppidSecretToken();
+        XmlReadWrite xmlReadWrite=new XmlReadWrite();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-        filePath = Server.MapPath("/XML/Set.config");
-            //如果文件存在，开始读取
-        if (File.Exists(filePath)) { 
-
-            //读取配置文件信息，并赋值给文本框 
-            FileStream fileStream=new FileStream(filePath,FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
-            XmlSerializer xmlSerializer=new XmlSerializer(typeof(AppidSecretToken));
-            appidSecret= xmlSerializer.Deserialize(fileStream) as AppidSecretToken;
-
-            txt_appid.Text = appidSecret.Appid;
-            txt_secret.Text = appidSecret.Secret;
-        }
+            _appidSecret = xmlReadWrite.Read("/XML/", _appidSecret, "set.config") as AppidSecretToken;
+            if (_appidSecret != null)
+            {
+                txt_appid.Text = _appidSecret.Appid;
+                txt_secret.Text = _appidSecret.Secret;
+                txt_token.Text = _appidSecret.Token;
+            }
         }
 
         /// <summary>
@@ -37,19 +28,16 @@ namespace WeiXin_Web
         /// <param name="e"></param>
         protected void btn_save_set_OnClick(object sender, EventArgs e)
         {
-            appidSecret.Appid = txt_appid.Text;
-            appidSecret.Secret = txt_secret.Text;
-            appidSecret.Token = txt_token.Text;
-            SaveConfig(appidSecret);
+            _appidSecret.Appid = txt_appid.Text;
+            _appidSecret.Secret = txt_secret.Text;
+            _appidSecret.Token = txt_token.Text;
+            SaveConfig(_appidSecret);
         }
 
 
         private void SaveConfig(object obj)
         {
-            filePath = Server.MapPath("/XML/Set.config");
-            FileStream fileStream=new FileStream(filePath,FileMode.Create,FileAccess.Write,FileShare.ReadWrite);
-            XmlSerializer xmlSerializer=new XmlSerializer(obj.GetType());
-            xmlSerializer.Serialize(fileStream,obj);
+            xmlReadWrite.Write("/XML/", obj, "set.config");
         }
     }
 }
