1831081b3d8bc85bd21f6e2518f3f865819cc418
马鑫 <anyangmaxin@163.com>
2015-04-20 10:45:59 +0800

    添加accessToken对象文件

---
+++ WeiXin_MX/WX_Tools/Entites/AccessToken.cs
@@ -0,0 +1,16 @@
+﻿namespace WX_Tools.Entites
+{
+    /// <summary>
+    /// access_token对象
+    /// </summary>
+  public  class AccessToken
+    {
+        private string _accessToken;
+        public string Access_token {
+            get { return _accessToken; }
+            set { _accessToken = value; }
+        }
+
+
+    }
+}
