4417890e9b81028f893ac4d7b4b3be334be666e4
马鑫 <anyangmaxin@163.com>
2015-04-20 14:20:11 +0800

    添加自定义方法类

--- WeiXin_MX/WeiXin_Web/Index.aspx.cs
+++ WeiXin_MX/WeiXin_Web/Index.aspx.cs
@@ -1,24 +1,29 @@
 ﻿using System;
-using System.Text;
-using System.Web;
 using System.Web.Configuration;
-using System.Web.Script.Serialization;
 using System.Web.UI;
 using WX_Tools;
 using WX_Tools.Entites;
+
+
+
+
+
 
 namespace WeiXin_Web
 {
     public partial class Index : Page
     {
         private AppidSecretToken _appidSecret=new AppidSecretToken();
-
+ 
         protected void Page_Load(object sender, EventArgs e)
         {
 
             //为AppidSecretToken对象赋值
             _appidSecret.Appid = WebConfigurationManager.AppSettings["appid"];
             _appidSecret.Secret = WebConfigurationManager.AppSettings["secret"];
+      
+
+
             }
         
 
@@ -56,10 +61,10 @@ namespace WeiXin_Web
         protected void btn_get_access_token_OnClick(object sender, EventArgs e)
         {
 
-            //  string accesstoken=new GetAccessToken().Get_access_token(_appidSecret,"catch");
-            string accesstoken = Get_access_token(_appidSecret, "catch");
-            new DebugLog().BugWriteTxt(new Log() { LogTxtPhyPath = "/ErrorTXT/" }.LogTxtPhyPath, accesstoken);
-            lab_access_token.Text = accesstoken;
+        
+            //string accesstoken = Get_access_token(_appidSecret, "catch");
+            //new DebugLog().BugWriteTxt(new Log() { LogTxtPhyPath = "/ErrorTXT/" }.LogTxtPhyPath, accesstoken);
+            //lab_access_token.Text = accesstoken;
         }
 
 
@@ -73,53 +78,13 @@ namespace WeiXin_Web
         /// <param name="e"></param>
         protected void btn_update_access_token_OnServerClick(object sender, EventArgs e)
         {
-            lab_access_token.Text = Get_access_token(_appidSecret, "server");
+           // lab_access_token.Text = Get_access_token(_appidSecret, "server");
+       
         }
 
 
 
 
-        /// <summary>
-        /// 取得access_token
-        /// </summary>
-        /// <returns></returns>
-        public string Get_access_token(AppidSecretToken appidSecret, string serverORcatch)
-        {
-
-            //access_token缓存名称
-            string accesstokenCachname = AllCach.AllCachEnum.AccessToken.ToString();
-           
-            //缓存类型
-            var accesstokenCache = HttpContext.Current.Cache[accesstokenCachname];
-
-            //从服务器获得access_token
-            if (serverORcatch.Equals("server"))
-            {
-                accesstokenCache = null;
-            }
-            
-
-            if (accesstokenCache == null)
-            {
-              
-               string resultJson=  new GetAccessToken().get_access_token(appidSecret);
-
-                JavaScriptSerializer javaScriptSerializer=new JavaScriptSerializer();
-               AccessToken accessToken= javaScriptSerializer.Deserialize(resultJson, typeof (AccessToken)) as AccessToken;
-
-
-               //把获取到access_token放入缓存中，设置失效时间为7000秒，比微信服务器上短一点就行
-               HttpContext.Current.Cache.Insert(AllCach.AllCachEnum.AccessToken.ToString(),
-                   accessToken.Access_token, null, DateTime.Now.AddSeconds(7000), TimeSpan.Zero);
-
-            }
-            
-
-
-           
-
-
-            return accesstokenCache.ToString();
-        }
+       
     }
 }
---
+++ WeiXin_MX/WeiXin_Web/MyClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Web;
+using System.Web.Script.Serialization;
+using WX_Tools;
+using WX_Tools.Entites;
+
+namespace WeiXin_Web
+{
+
+
+
+    public class MyClass
+    {
+     
+
+        /// <summary>
+        /// 取得access_token
+        /// </summary>
+        /// <returns></returns>
+        public string Get_access_token(AppidSecretToken appidSecret, string serverORcatch)
+        {
+
+            //access_token缓存名称
+            string accesstokenCachname = AllCach.AllCachEnum.AccessToken.ToString();
+
+            //缓存类型
+            object accesstokenCache = HttpContext.Current.Cache[accesstokenCachname];
+
+
+
+            //从服务器获得access_token
+            if (serverORcatch.Equals("server"))
+            {
+                accesstokenCache = null;
+            }
+
+
+            if (accesstokenCache == null)
+            {
+
+                string resultJson = new GetAccessToken().get_access_token(appidSecret);
+
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                AccessToken accessToken = javaScriptSerializer.Deserialize(resultJson, typeof(AccessToken)) as AccessToken;
+
+
+                //把获取到access_token放入缓存中，设置失效时间为7000秒，比微信服务器上短一点就行
+                HttpContext.Current.Cache.Insert(AllCach.AllCachEnum.AccessToken.ToString(),
+                    accessToken.Access_token, null, DateTime.Now.AddSeconds(7000), TimeSpan.Zero);
+
+                accesstokenCache = HttpContext.Current.Cache[accesstokenCachname];
+
+            }
+
+
+            return accesstokenCache.ToString();
+        }
+
+    }
+
+}
+
